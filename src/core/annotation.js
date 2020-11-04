cc6914e0148b5ee7fee6bb4e62df8adf61fa06ee
horelvis <hcastillo.mendoza@gmail.com>
2020-11-04 14:39:48 +0100

    Commented data.fieldType === "Sig")

--- src/core/annotation.js
+++ src/core/annotation.js
@@ -983,9 +983,9 @@ class WidgetAnnotation extends Annotation {
     // since it's (most likely) a `Dict` which is non-serializable and will thus
     // cause errors when sending annotations to the main-thread (issue 10347).
     if (data.fieldType === "Sig") {
-      data.fieldValue = null;
-      this.setFlags(AnnotationFlag.HIDDEN);
-      data.hidden = true;
+    //  data.fieldValue = null;
+    //  this.setFlags(AnnotationFlag.HIDDEN);
+    //  data.hidden = true;
     }
   }
 
